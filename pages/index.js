ef62eb33c1cd479c6cdcb093aaad7ab1cd5771f8
MJ174 <audwls714@gmail.com>
2022-10-17 21:50:58 +0900

    upload file

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title> Home - Next Tutorial</title>
+      </Head>
+      <Link href="about">About</Link>
+      <h1 Welcome className={styles.homeTitle}>
+        Welcome Home
+      </h1>
+    </div>
+  );
+}
